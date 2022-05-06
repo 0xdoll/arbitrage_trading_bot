b258c32916ac224ba87f7f82b9e50af697862570
0xdoll <secdollg@gmail.com>
2022-05-06 15:37:43 +0800

    harhat deploy scripts.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,14 @@
 const hre = require("hardhat");
+const config = require("../config.json")
 
 async function main() {
   await hre.run('compile');
 
-  const Contract = await hre.ethers.getContractFactory("ArbitrageBot");
-  const contract = await Contract.deploy();
-  await contract.deployed();
+  const ArbitrageBot = await hre.ethers.getContractFactory("ArbitrageBot");
+  const arbitrageBot = await ArbitrageBot.deploy();
+  await arbitrageBot.deployed(config.SUSHISWAP.V2_ROUTER_02_ADDRESS, config.UNISWAP.V2_ROUTER_02_ADDRESS);
 
-  console.log("Contract deployed to:", helloer.address);
-
-  // await hre.run("verify:verify", {
-  //   address: contract.address,
-  //   constructorArguments: [
-  //       "",
-  //   ],
-  // });
-
+  console.log("arbitrageBot deployed to:", helloer.address);
 }
 
 main()
